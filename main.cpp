b14b55ac4e60e0467dcb6029755b7b979cea972a
chlee <z000000375@wemakeprice.com>
2023-05-17 01:10:15 +0900

    .

--- main.cpp
+++ main.cpp
@@ -2,31 +2,42 @@
 #include <vector>
 #include <cstring>
 
+struct ProcessedResult {
+    const int* result_vec;
+    size_t result_len;
+    const int* mask_vec;
+    size_t mask_len;
+    std::vector<int> result_box;
+    std::vector<int> mask_box;
+};
+
 extern "C" {
-std::vector<int>* process_string(const char* input);
-size_t get_result_size(const std::vector<int>* result);
-const int* get_result_data(const std::vector<int>* result);
-void free_result(std::vector<int>* result);
+ProcessedResult* process_string(const char* input);
+void free_processed_result(ProcessedResult* result);
 }
 
 int main() {
     // Call the Rust function
     const char* input = "Hello, Rust!";
-    std::vector<int>* result = process_string(input);
+    ProcessedResult* result = process_string(input);
 
     if (result) {
-        // Get the size of the result vector
-        size_t result_size = get_result_size(result);
-
-        // Access and print the vector elements
-        const int* result_data = get_result_data(result);
-        for (size_t i = 0; i < result_size; ++i) {
+        // Access and print the result vector elements
+        const int* result_data = result->result_vec;
+        for (size_t i = 0; i < result->result_len; ++i) {
             std::cout << result_data[i] << " ";
         }
         std::cout << std::endl;
 
-        // Free the allocated vector memory
-        free_result(result);
+        // Access and print the mask vector elements
+        const int* mask_data = result->mask_vec;
+        for (size_t i = 0; i < result->mask_len; ++i) {
+            std::cout << mask_data[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // Free the allocated memory
+        free_processed_result(result);
     } else {
         std::cout << "Error occurred while calling the Rust function." << std::endl;
     }
